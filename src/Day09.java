be82212c94333ab098facbe19416aee9e1ad51ed
Bailey <bailey.uniacke.03@gmail.com>
2022-12-10 17:57:07 +0000

    Complete day 9 part 2

--- src/Day09.java
+++ src/Day09.java
@@ -9,9 +9,7 @@ public class Day09 {
         Integer[] head = {0,0};
         Integer[] tail = {0,0};
         HashSet<List<Integer>> positionHistory = new HashSet<>();
-        int counter = 0;
         while (reader.hasNextLine()) {
-            counter ++;
             String line = reader.nextLine();
             String[] splitLine = line.split(" ");
             for (int i = 0; i < Integer.parseInt(splitLine[1]); i ++) {
@@ -21,7 +19,6 @@ public class Day09 {
                     case "R" -> head[0]++;
                     case "L" -> head[0]--;
                 }
-                System.out.println(Arrays.toString(head));
                 if (Objects.equals(head[0], tail[0])) {
                     // Same column. Move 1 or 0 positions left or right closer to head
                     tail[1] += (head[1] - tail[1]) / 2;
@@ -36,8 +33,48 @@ public class Day09 {
                         tail[1] += yDiff >= 1 ? 1 : -1;
                     }
                 }
-                System.out.println(Arrays.toString(tail));
-                System.out.println();
+                positionHistory.add(List.of(tail));
+            }
+        }
+        return positionHistory.size();
+    }
+
+    public static int part02 (File file) throws FileNotFoundException {
+        Scanner reader = new Scanner(file);
+        Integer[][] rope = new Integer[10][2];
+        for (int i = 0; i < rope.length; i++) rope[i] = new Integer[]{0, 0};
+        System.out.println(Arrays.deepToString(rope));
+        HashSet<List<Integer>> positionHistory = new HashSet<>();
+        while (reader.hasNextLine()) {
+            String line = reader.nextLine();
+            String[] splitLine = line.split(" ");
+            for (int i = 0; i < Integer.parseInt(splitLine[1]); i ++) {
+                Integer[] head = rope[0];
+                Integer[] tail = rope[9];
+                switch (splitLine[0]) {
+                    case "U" -> head[1]++;
+                    case "D" -> head[1]--;
+                    case "R" -> head[0]++;
+                    case "L" -> head[0]--;
+                }
+                for (int j = 1; j < rope.length; j++) {
+                    Integer[] leader = rope[j-1];
+                    Integer[] follower = rope[j];
+                    if (Objects.equals(leader[0], follower[0])) {
+                        // Same column. Move 1 or 0 positions left or right closer to leader
+                        follower[1] += (leader[1] - follower[1]) / 2;
+                    } else if (Objects.equals(leader[1], follower[1])) {
+                        follower[0] += (leader[0] - follower[0]) / 2;
+                    } else {
+                        // Must move diagonally
+                        int xDiff = leader[0] - follower[0];
+                        int yDiff = leader[1] - follower[1];
+                        if (!(Math.abs(xDiff) == 1 && Math.abs(yDiff) == 1)) {
+                            follower[0] += xDiff >= 1 ? 1 : -1;
+                            follower[1] += yDiff >= 1 ? 1 : -1;
+                        }
+                    }
+                }
                 positionHistory.add(List.of(tail));
             }
         }
@@ -48,5 +85,7 @@ public class Day09 {
         File file = new File("docs/Day09.txt");
         int part01 = part01(file);
         System.out.println(part01);
+        int part02 = part02(file);
+        System.out.println(part02);
     }
 }
